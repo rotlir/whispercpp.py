e9494c40484fbe2b1d1fb55b6ed258809c1c1581
rotlir <131175687+rotlir@users.noreply.github.com>
2024-03-12 10:14:06 +0300

    add flags for arm

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
-import numpy, os, sys
+import numpy, os, sys, platform
 
 if sys.platform == 'darwin':
     os.environ['CFLAGS']   = '-DGGML_USE_ACCELERATE -O3 -std=gnu11'
     os.environ['CXXFLAGS'] = '-DGGML_USE_ACCELERATE -O3 -std=c++11'
     os.environ['LDFLAGS']  = '-framework Accelerate'
-else:
+elif "arm" not in platform.machine():
     os.environ['CFLAGS']   = '-mavx -mavx2 -mfma -mf16c -O3 -std=gnu11'
     os.environ['CXXFLAGS'] = '-mavx -mavx2 -mfma -mf16c -O3 -std=c++11'
+else:
+    os.environ['CFLAGS'] = '-mfpu=neon -mfloat-abi=hard -O3 -std=gnu11'
+    os.environ['CXXFLAGS'] = '-mfpu=neon -mfloat-abi=hard -O3 -std=c++11'
 
 ext_modules = [
     Extension(
