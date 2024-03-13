ee8b33c5fe42bead0a163e59a9607dee30f77d23
rotlir <131175687+rotlir@users.noreply.github.com>
2024-03-13 10:17:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ elif "arm" not in platform.machine():
     os.environ['CFLAGS']   = '-mavx -mavx2 -mfma -mf16c -O3 -std=gnu11'
     os.environ['CXXFLAGS'] = '-mavx -mavx2 -mfma -mf16c -O3 -std=c++11'
 else:
-    os.environ['CFLAGS'] = '-mfpu=neon -mfp16-format=ieee -mno-unaligned-access -O3 -std=gnu11'
-    os.environ['CXXFLAGS'] = '-mfpu=neon -mfp16-format=ieee -mno-unaligned-access -O3 -std=c++11'
+    os.environ['CFLAGS'] = '-mfpu=neon-fp-armv8 -mfp16-format=ieee -funsafe-math-optimizations -mno-unaligned-access -O3 -std=gnu11'
+    os.environ['CXXFLAGS'] = '-mfpu=neon-fp-armv8 -mfp16-format=ieee -funsafe-math-optimizations -mno-unaligned-access -O3 -std=c++11'
 
 ext_modules = [
     Extension(
